0eb073bbe15ab8b84581cff7006dc86a4126962a
baxromsafarov <drunk3065@gmail.com>
2025-06-13 15:12:55 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
+
+var actions = []string{
+	"logged in",
+	"logged out",
+	"create record",
+	"update record",
+	"delete record",
+}
+
+type LogItem struct {
+	action    string
+	timestamp time.Time
+}
+
+type User struct {
+	id    int
+	email string
+	logs  []LogItem
+}
+
+func (u User) getActivityInfo() string {
+	out := fmt.Sprintf("ID: %d | Email: %s\nActivity Log:\n", u.id, u.email)
+	for i, item := range u.logs {
+		out += fmt.Sprintf("%d. [%s] at %s\n", i+1, item.action, item.timestamp)
+	}
+	return out
+}
+
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	t := time.Now()
+
+	wg := &sync.WaitGroup{}
+
+	users := generateUsers(1000)
+	for _, user := range users {
+		wg.Add(1)
+		go saveUserInfo(user, wg)
+	}
+
+	wg.Wait()
+
+	fmt.Println("Time since last update:", time.Since(t).String())
+
+}
+
+func saveUserInfo(user User, wg *sync.WaitGroup) error {
+	time.Sleep(time.Millisecond * 10)
+	fmt.Printf("WRITING FILE FOR USER ID : %d\n", user.id)
+
+	filename := fmt.Sprintf("logs/user-%d.txt", user.id)
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	//defer file.Close()
+	_, err = file.WriteString(user.getActivityInfo())
+	if err != nil {
+		return err
+	}
+
+	wg.Done()
+
+	return nil
+
+}
+
+func generateUsers(count int) []User {
+	users := make([]User, count)
+	for i := 0; i < count; i++ {
+		users[i] = User{
+			id:    i + 1,
+			email: fmt.Sprintf("user%d@ninja.go", i+1),
+			logs:  generateLogs(rand.Intn(1000)),
+		}
+	}
+	return users
+}
+
+func generateLogs(count int) []LogItem {
+	logs := make([]LogItem, count)
+
+	for i := 0; i < count; i++ {
+		logs[i] = LogItem{
+			timestamp: time.Now(),
+			action:    actions[rand.Intn(len(actions)-1)],
+		}
+	}
+	return logs
+}
